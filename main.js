74903383ad462ee319205bd5c4283b662d1bd607
kundanch240 <kundanch8181@gmail.com>
2025-05-19 08:38:56 +0530

    my portfolio with side menu

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log('Hello World!');
+
+
+// nav bar functionality selection start
+
+const sidemenu = document.querySelector('.bar')
+const bar1 = document.querySelector('#bar1')
+const bar2 = document.querySelector('#bar2')
+const bar3 = document.querySelector('#bar3')
+const header = document.querySelector('#side-logic')
+const mainul = document.querySelector('header nav ul')
+
+
+// nav bar selection end
+
+
+// main nav function start
+
+sidemenu.addEventListener("click", () => {
+  bar1.classList.toggle('barline1')
+  bar2.classList.toggle('barline2')
+  bar3.classList.toggle('barline3')
+  sidemenu.classList.toggle('main-bar')
+  header.classList.toggle('side-head')
+  mainul.classList.toggle('main-ul')
+});
+
+// main nav function end
